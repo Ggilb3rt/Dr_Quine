c090abeb28d6feec252be38265f94d162c867c78
Ggilb3rt <joeletouffu@gmail.com>
2023-01-31 14:36:22 +0100

    Sully is ok

--- 2_Sully/C/Sully.c
+++ 2_Sully/C/Sully.c
@@ -1,27 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(void){
-	int	i = 5;
-	
-	if (i >= 0){
-		char	child_name[10];
-		char	cmd_exec_child[100];
-		FILE	*child_file;
-
-		sprintf(child_name, "Sully_%d.c", i - 1);
-		child_file = fopen(child_name, "wx");
-		if (child_file != ((void *)0)){
-			fprintf(child_file, "#include <stdio.h>\n#include <stdlib.h>\n\nint main(void){int i = %d; printf(%c%s%c, i);return (0);}", i - 1, 34, "Hello there %d!", 34);
-			fclose(child_file);
-			sprintf(cmd_exec_child, "bash clang -Werror -Wextra -Wall %s -o Sully_child", child_name);
-			printf("%s\n", cmd_exec_child);
-			system("ls -la");
-			if (i > 0)
-				// need uid
-				if (system(cmd_exec_child))
-					system("bash ./Sully_child");
-		}
-	}
-	return (0);
-}
+int main(void){int i = 5;if(i > 0){
+	char *a="#include <stdio.h>%c#include <stdlib.h>%cint main(void){int i = %d;if(i > 0){%c%cchar *a=%c%s%c;%c%cchar child_name[10]; char cmd_exec_child[100]; FILE *child_file;%c%csprintf(child_name, %cSully_%%d.c%c, i - 1); child_file = fopen(child_name, %cwx%c);%c%cif (child_file != ((void *)0)){%c%c%cfprintf(child_file, a, 10, 10, i - 1, 10, 9, 34, a, 34, 10, 9, 10, 9, 34, 34, 34, 34, 10, 9, 10, 9, 9, 10, 9, 9, 34, 34, 10, 9, 9, 10); fclose(child_file);%c%c%csprintf(cmd_exec_child, %cgcc %%s -o Sully_child_%%d;./Sully_child_%%d;%c, child_name, i - 1, i - 1);%c%c%cif (i > 0) system(cmd_exec_child);}%c}return (0);}";
+	char child_name[10]; char cmd_exec_child[100]; FILE *child_file;
+	sprintf(child_name, "Sully_%d.c", i - 1); child_file = fopen(child_name, "wx");
+	if (child_file != ((void *)0)){
+		fprintf(child_file, a, 10, 10, i - 1, 10, 9, 34, a, 34, 10, 9, 10, 9, 34, 34, 34, 34, 10, 9, 10, 9, 9, 10, 9, 9, 34, 34, 10, 9, 9, 10); fclose(child_file);
+		sprintf(cmd_exec_child, "gcc %s -o Sully_child_%d;./Sully_child_%d;", child_name, i - 1, i - 1);
+		if (i > 0) system(cmd_exec_child);}
+}return (0);}
--- 2_Sully/C/Sully_4.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void){int i = 4; printf("Hello there %d!", i);return (0);}
