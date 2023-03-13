7c38bdf22c00e971ea24f1c99052fba5f0fa0b7e
Ggilb3rt <joeletouffu@gmail.com>
2023-03-13 15:14:15 +0100

    Sully works and makefile don't crash with diff

--- 2_Sully/C/Sully.c
+++ 2_Sully/C/Sully.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(void){int i = 5;if(i + 1){
-	char *a="#include <stdio.h>%c#include <stdlib.h>%cint main(void){int i = %d;if(i + 1){%c%cchar *a=%c%s%c;%c%cchar child_name[10]; char cmd_exec_child[100]; FILE *child_file;%c%csprintf(child_name, %cSully_%%d.c%c, i); child_file = fopen(child_name, %cwx%c);%c%cif (child_file != ((void *)0)){i--;%c%c%cfprintf(child_file, a, 10, 10, i, 10, 9, 34, a, 34, 10, 9, 10, 9, 34, 34, 34, 34, 10, 9, 10, 9, 9, 10, 9, 9, 34, 34, 10, 9, 9, 10); fclose(child_file);%c%c%csprintf(cmd_exec_child, %cgcc %%s -o Sully_child_%%d;./Sully_child_%%d;%c, child_name, i, i);%c%c%cif (i + 1) system(cmd_exec_child);}%c}return (0);}";
+#include <unistd.h>
+int main(void){
+	int i = 5;
+	if(i + 1){
+	char *a="#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <unistd.h>%1$cint main(void){%1$c%3$cint i = %4$d;%1$c%3$cif(i + 1){%1$c%3$cchar *a=%2$c%5$s%2$c;%1$c%3$cchar child_name[10]; char cmd_exec_child[100]; FILE *child_file;%1$c%3$cif (access(%2$cSully_5.c%2$c, R_OK) != -1) i--;%1$c%3$csprintf(child_name, %2$cSully_%%d.c%2$c, i);%1$c%3$cchild_file = fopen(child_name, %2$cwx%2$c);%1$c%3$cif (child_file != ((void *)0)){%1$c%3$c%3$cfprintf(child_file, a, 10, 34, 9, i, a); fclose(child_file);%1$c%3$c%3$csprintf(cmd_exec_child, %2$cgcc %%s -o Sully_%%d;./Sully_%%d;%2$c, child_name, i, i);%1$c%3$c%3$cif (i) system(cmd_exec_child);}%1$c}return (0);}";
 	char child_name[10]; char cmd_exec_child[100]; FILE *child_file;
+	if (access("Sully_5.c", R_OK) != -1) i--;
 	sprintf(child_name, "Sully_%d.c", i);
 	child_file = fopen(child_name, "wx");
 	if (child_file != ((void *)0)){
-		i--;
-		fprintf(child_file, a, 10, 10, i, 10, 9, 34, a, 34, 10, 9, 10, 9, 34, 34, 34, 34, 10, 9, 10, 9, 9, 10, 9, 9, 34, 34, 10, 9, 9, 10); fclose(child_file);
-		sprintf(cmd_exec_child, "gcc %s -o Sully_child_%d;./Sully_child_%d;", child_name, i, i);
-		if (i + 1) system(cmd_exec_child);}
+		fprintf(child_file, a, 10, 34, 9, i, a); fclose(child_file);
+		sprintf(cmd_exec_child, "gcc %s -o Sully_%d;./Sully_%d;", child_name, i, i);
+		if (i) system(cmd_exec_child);}
 }return (0);}
